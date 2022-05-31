7aa95b871d289dd87d510ee1e97f4a7db9883244
JonasRegener <regener.jonas@gmail.com>
2022-05-31 22:05:21 +0200

    added more stuff and url to portfolio

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  changeText: boolean;
+  changeText2: boolean;
+  changeText3: boolean;
+  changeText4: boolean;
 
-  constructor() { }
+  constructor() { this.changeText = false;
+    this.changeText2 = false;
+    this.changeText3 = false;
+    this.changeText4 = false; }
 
   ngOnInit(): void {
+
   }
 
 }
